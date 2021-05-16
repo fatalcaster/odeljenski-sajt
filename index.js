62983713633d82a203b31a9d424bc33bee456076
fatalcaster <60295118+fatalcaster@users.noreply.github.com>
2021-05-16 13:58:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+
+app.use(expressLayouts);
+app.use(methodOverride("_method"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
